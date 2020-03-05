7317eb425755d7d6dded03e8f980cf436527e425
Lucas Adorno <lucasadorno16@hotmail.com>
2020-03-04 21:27:01 -0300

    Corrigido a url de conexão do mongoose

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,14 +7,18 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(
-      `mongodb://${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`,
-      {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-      }
-    );
+    this.mongoConnection = mongoose
+      .connect(
+        `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+        {
+          useNewUrlParser: true,
+          useFindAndModify: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+        }
+      )
+      .then(() => console.log('working'))
+      .catch(err => console.log(err));
   }
 }
 
