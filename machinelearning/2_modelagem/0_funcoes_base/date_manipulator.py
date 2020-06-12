a3a41dc143b45ce45b85775ab7ed3f09f4ac6b7d
Lucas Adorno <lucasadorno16@hotmail.com>
2020-06-12 09:55:38 -0300

    Adicionado métodos auxiliares

--- machinelearning/2_modelagem/0_funcoes_base/date_manipulator.py
+++ machinelearning/2_modelagem/0_funcoes_base/date_manipulator.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def ticks_to_datetime(ticks):
   return datetime.fromtimestamp(ticks/1000.0)
 
-def get_date_window(start_date, end_date, mapping_window_step=100, mapping_window_step_unit='ms', bound=(None,None), closed=None):
+def get_date_window(start_date, end_date, mapping_window_step=100, mapping_window_step_unit='ms', bound=(None,None), closed=None, to_pydatetime=True):
   available_min_timestamp = bound[0]
   available_max_timestamp = bound[1]
 
@@ -17,4 +17,9 @@ def get_date_window(start_date, end_date, mapping_window_step=100, mapping_windo
   if available_max_timestamp:
     max_range = max_range if max_range <= available_max_timestamp else available_max_timestamp
   
-  return pd.date_range(start=min_range, end=max_range, freq=str(mapping_window_step) + ' ' + mapping_window_step_unit, closed=closed).to_pydatetime()
+  result = pd.date_range(start=min_range, end=max_range, freq=str(mapping_window_step) + ' ' + mapping_window_step_unit, closed=closed)
+  
+  if to_pydatetime:
+    return result.to_pydatetime()
+  
+  return result
--- machinelearning/2_modelagem/0_funcoes_base/file_manipulator.py
+++ machinelearning/2_modelagem/0_funcoes_base/file_manipulator.py
@@ -1,19 +1,26 @@
 import os
 
-def get_out_filename(out_prefix, out_ref_dir, in_filename):
+def get_out_filename(prefix, ref_dir, in_filename):
   result = ''
-  prefix = ''
 
-  if not out_prefix:
-    out_prefix = ''
-  if not out_ref_dir:
-    out_ref_dir = './'
+  if not prefix:
+    prefix = ''
+  if not ref_dir:
+    ref_dir = './'
   if not in_filename:
     return result
 
   in_prefix_idx = in_filename.find('_')
+  in_prefix = ''
   if in_prefix_idx > 0 and in_prefix_idx <= 3:
-      prefix = in_filename[:in_prefix_idx + 1]
+      in_prefix = in_filename[:in_prefix_idx + 1]
   
-  result = os.path.join(out_ref_dir, out_prefix + in_filename.replace(prefix, ''))
+  result = os.path.join(ref_dir, prefix + in_filename.replace(in_prefix, ''))
   return result
+
+def remove_prefix(filename_without_path):
+  in_prefix_idx = filename_without_path.find('_')
+  in_prefix = ''
+  if in_prefix_idx > 0 and in_prefix_idx <= 3:
+      in_prefix = filename_without_path[:in_prefix_idx + 1]
+  return filename_without_path.replace(in_prefix, '')
